1ad32b56968f11c6775ce248f76f7b915ab7a8f2
Nieldaw <nielda1494@gmail.com>
2025-02-25 19:32:07 +0100

    Add scss support

--- functions.php
+++ functions.php
@@ -7,8 +7,9 @@ defined('WPINC') || wp_die();
 // Salient styles
 // -----------------------------------------------------------------------------
 add_action('wp_enqueue_scripts', function () {
-    wp_enqueue_style('fpimmo-style', get_stylesheet_directory_uri() . '/css/main.css', '', );
+    wp_enqueue_style('fpimmo-style', get_stylesheet_directory_uri() . '/dist/css/main.css', '', );
 
 
     wp_enqueue_script('jquery', 'https://code.jquery.com/jquery-3.7.1.min.js', [], '3.7.1');
+    wp_enqueue_script('child-js', get_stylesheet_directory_uri() . '/dist/js/main.js', ['jquery'], '1.0.0');
 }, 100);
