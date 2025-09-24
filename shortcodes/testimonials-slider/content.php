0164060189e48882cd6c8f9d3984df1655e314f4
fvillard <fvillard@coachit.ch>
2025-09-24 22:00:37 +0200

    Add base of shortcode

---
+++ shortcodes/testimonials-slider/content.php
@@ -0,0 +1,17 @@
+<?php
+// If this file is called directly, abort.
+defined('WPINC') || wp_die();
+
+?>
+<div class="testimonials-slider">
+    testimonials-slider
+    <pre>
+        <?php print_r($items); ?>
+    </pre>
+</div>
+
+<script>
+    jQuery(function ($) {
+       
+    });
+</script>
