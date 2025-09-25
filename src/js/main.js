6c3585ce6748eea9db8f7080dd61511b233275b5
fvillard <fvillard@coachit.ch>
2025-09-25 14:50:33 +0200

    Add flickity initialisation + UI

--- src/js/main.js
+++ src/js/main.js
@@ -3,4 +3,14 @@ jQuery(document).ready(function ($) {
     delegate: "div",
     type: "image",
   });
+
+  if ($(".testimonials-slider").length) {
+    $(".flickity-wrapper").flickity({
+      wrapAround: true,
+      autoPlay: true,
+      pageDots: false,
+      cellAlign: "center",
+      adaptiveHeight: true,
+    });
+  }
 });
